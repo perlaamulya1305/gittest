26d0651c8596f19136e5a4769e9fb376b4193f3b
perlaamulya <32683110+perlaamulya@users.noreply.github.com>
2018-01-04 14:33:35 -0500

    adding an extra code

--- src/test/java/GitSample.java
+++ src/test/java/GitSample.java
@@ -3,5 +3,6 @@ import javax.xml.bind.SchemaOutputResolver;
 public class GitSample {
     public static void main(String Args[]){
         System.out.println("Sample Git Program");
+        System.out.println("changes made");
     }
 }
